2573f8251cad13780414acc83bcd76f4966399de
Michele Sordo <michele.sordo@gmail.com>
2014-11-05 15:41:53 +0100

    javascript

---
+++ javascript_triangles/script.js
@@ -0,0 +1,24 @@
+function isTriangle(a,b,c) {
+	return ((a+b)>c) && (a+c>b) && (b+c > a);
+}
+
+function getTriangleType (a,b,c) {
+	if(a==b==c)
+		return "equilater";
+	else if((a!=b)&&(a!=c)&&(b!=c))
+		return "scalene";
+	else
+		return "isosceles";
+}
+
+function sumAll(array) {
+	sum = 0;
+	array.forEach(function(value) {
+    	sum+=value;
+	});
+	return sum;
+}
+
+function aggregateAll(array, aggrFunc) {
+	return aggrFunc(array);
+}
